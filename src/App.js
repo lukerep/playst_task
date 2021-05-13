5bace76124f49ff3f8a87372e17e7196de2218e0
Luke Rep-Baihelfer <lukerep97@gmail.com>
2021-05-14 09:16:37 +1000

    implemented white boxes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="body-container">
+        <div className="panel panel-lrg">1</div>
+        <div className="panel panel-med">2</div>
+        <div className="panel panel-sml">3</div>
+        <div className="panel panel-sml">4</div>
+        <div className="panel panel-sml">5</div>
+      </div>
     </div>
   );
 }
